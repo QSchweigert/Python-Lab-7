f6b61846bca5810f76cc9a055ba5ce2fcbda6807
qschweigert062 <quinnscheigert@gmail.com>
2025-10-16 12:34:09 -0700

    LAB 07

--- Lab007.py
+++ Lab007.py
@@ -1,5 +1,5 @@
-#alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
+#alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 alpha_len = len(alphabet)
 
 def pretty_print(vsq: list):
@@ -62,11 +62,36 @@ def vigenere_index(key_letter: str ,
          letter_to_index(plaintext_letter, alphabet)) % alpha_len)
     return index_to_letter(ci , alphabet)
 
+def undo_vigenere_index(key_letter: str,
+                        cipher_letter:str,
+                        alphabet: str) ->str:
+    #C = (k + p) % AL
+    #P = (C - K) % AL
+    pi = ((letter_to_index(cipher_letter, alphabet) -
+           letter_to_index(key_letter, alphabet)) % alpha_len)
+    return index_to_letter(pi, alphabet)
+
 def encrypt_vigenere(key, plaintext, alphabet):
-        cipher_text = ''
+        cipher_text = []
         for i, pt in enumerate(plaintext):
-            cipher_text += vigenere_index(key[i%len(key)], pt, alphabet)
-        return cipher_text
+            cipher_text.append(
+            vigenere_index(key[i%len(key)],
+                    pt,
+                    alphabet))
+        return ''.join(cipher_text)
+
+def decrypt_vigenere(key: str, ciphertext: str, alphabet: str):
+    plain_text = []
+    for i, ct in enumerate(ciphertext):
+        plain_text.append(
+            undo_vigenere_index(key[i % len(key)],
+                           ct,
+                           alphabet))
+    return ''.join(plain_text)
+
 #print(vigenere_index("D", "T", alphabet))
-print(encrypt_vigenere(key, plaintext, alphabet))
-# WHZHRCOOEUPNUHOAHLRF
+et = encrypt_vigenere(key, plaintext, alphabet)
+# vgyHqbnndUomtHn Hkqe
+pt = decrypt_vigenere(key, et, alphabet)
+print(et, pt)
+
